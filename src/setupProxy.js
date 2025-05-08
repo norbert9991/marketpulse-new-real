15002d0e3039784929a8c8ccd1faec0a80e0a774
Norbert Cabasag <145771756+norbert9991@users.noreply.github.com>
2025-05-08 17:02:31 +0800

    Add files via upload

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  const target = process.env.REACT_APP_API_URL || 'https://marketpulse-new-real-2-0.onrender.com/api';
+  const target = process.env.REACT_APP_API_URL || 'https://marketpulse-new-real-3-web.onrender.com/api';
   
   app.use(
     '/api',
